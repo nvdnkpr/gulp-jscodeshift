54ab316f37957f6c4f6c7a52bff90f99a139d422
Navid Nikpour <navid@nikpour.com>
2017-11-03 13:22:38 +0100

    Restructures test files and updates gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ const nodeunit = require('gulp-nodeunit');
 
 gulp.task('clean', () => del(['tmp/**']));
 
-gulp.task('copy', ['clean'], () => gulp.src('tests/fixtures/code.js',{base: 'tests/fixtures/'}).pipe(gulp.dest('tmp')));
+gulp.task('copy', ['clean'], () => gulp.src('tests/fixtures/code.js', { base: 'tests/fixtures/' }).pipe(gulp.dest('tmp')));
 
-gulp.task('jscodeshift', ['copy'], () => gulp.src('tmp/*.js').pipe(jscodeshift('./tests/fixtures/transform.js')));
+gulp.task('jscodeshift', ['copy'], () => gulp.src('tmp/*.js').pipe(jscodeshift('tests/transforms/reverse-transform.js')));
 
-gulp.task('nodeunit', ['jscodeshift'], () => gulp.src('tests/**/*.test.js').pipe(nodeunit({reporterOptions: {output: 'test'}})));
+gulp.task('nodeunit', ['jscodeshift'], () => gulp.src('tests/**/*.spec.js').pipe(nodeunit({ reporterOptions: { output: 'test' } })));
 
-gulp.task('test', ['clean', 'copy', 'jscodeshift', 'nodeunit']);
+gulp.task('test', ['nodeunit', 'clean']);
---
+++ tests/transforms/reverse-transform.js
@@ -0,0 +1,8 @@
+module.exports = function transformer(file, api) {
+    const j = api.jscodeshift;
+
+    return j(file.source)
+        .find(j.Identifier)
+        .replaceWith((p) => j.identifier(p.node.name.split('').reverse().join('')))
+        .toSource({ lineTerminator: '\n' });
+};
