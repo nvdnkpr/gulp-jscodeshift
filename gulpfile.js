1b405444c215a6d23b09af42bb479dab97ae47d4
Navid Nikpour <navid@nikpour.com>
2017-11-06 17:10:27 +0100

    Updates gulpfile, reduce tasks to only deleting compiled files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,4 @@
 const gulp = require('gulp');
-const copy = require('gulp-copy');
 const del = require('del');
-const jscodeshift = require('./');
-const nodeunit = require('gulp-nodeunit');
 
-gulp.task('clean', () => del(['tmp/**']));
-
-gulp.task('copy', ['clean'], () => gulp.src('tests/fixtures/code.js', { base: 'tests/fixtures/' }).pipe(gulp.dest('tmp')));
-
-gulp.task('jscodeshift', ['copy'], () => gulp.src('tmp/*.js').pipe(jscodeshift('tests/transforms/reverse-transform.js')));
-
-gulp.task('nodeunit', ['jscodeshift'], () => gulp.src('tests/**/*.spec.js').pipe(nodeunit({ reporterOptions: { output: 'test' } })));
-
-gulp.task('test', ['nodeunit', 'clean']);
+gulp.task('clean', () => del(['index.js', 'index.js.map', 'lib']));
