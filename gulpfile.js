4e2c7906397b680562bf3f7b6d3660709ce6f3e6
Navid Nikpour <navid@nikpour.com>
2018-01-15 11:07:43 +0100

    Changes build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 const gulp = require('gulp');
 const del = require('del');
+const ts = require('gulp-typescript');
 
-gulp.task('clean', () => del(['index.*', 'lib']));
+// cleans index files in root dir and dist folder
+gulp.task('clean', () => {
+  return del(['index.*', 'dist']);
+});
+
+// copies custom declaration types to dist folder
+gulp.task('copy-types', () => {
+  return gulp.src('src/@types/**', { base: 'src' }).pipe(gulp.dest('dist'));
+});
+
+// compiles ts files to dist folder
+gulp.task('build', ['copy-types'], () => {
+  const tsProject = ts.createProject('tsconfig.json');
+  return gulp.src('src/**/*.ts').pipe(tsProject()).pipe(gulp.dest('dist'));
+});
